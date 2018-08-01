ddad0ef2ec28cb3b32ff25635c49c1196031ee76
Nikita Lesnikov <n_lesnikov@nlmbp.local>
2018-08-01 18:22:22 +0300

    Typescript mode.

---
+++ .emacs.d/elpa/typescript-mode-20180801.850/typescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typescript-mode" "20180801.850" "Major mode for editing typescript" 'nil :commit "9796425fdb7d95463950e1277e2f3f9805ba0947" :keywords '("typescript" "languages") :url "http://github.com/ananthakumaran/typescript.el")
