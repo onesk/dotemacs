8cc198717a63b4385434d3e67320c80e8683d1d2
Nikita Lesnikov <one.devl@gmail.com>
2021-09-20 00:41:38 +0300

    Moving to use-package, wip.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,26 +1,32 @@
-;; inherit bashrc defaults
-
-;; (defun import-shell-var (name set-exec)
-;;   (let ((value-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string (concat "$SHELL --login -i -c 'echo $" name "'")))))
-;;     (setenv name value-from-shell)
-;;     (when set-exec
-;;       (setq exec-path (split-string value-from-shell path-separator)))))
-
-;; (import-shell-var "PATH" t)
-;; (import-shell-var "RUST_SRC_PATH" nil)
-
-;; org-mode by default
-(setq-default major-mode 'org-mode)
-
 ;; ELPA / MELPA setup
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
-(package-initialize)
+;; use-package
+(setq package-archive-priorities
+      '(("melpa" . 20)
+	("gnu" . 10)))
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq use-package-always-ensure t)
+
+;; binding & modeline
+(use-package diminish)
+(use-package bind-key)
+
+;; no visual clutter
+(menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+;; org-mode by default
+(setq-default major-mode 'org-mode)
 
 ;; shorter predicate
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -29,16 +35,16 @@
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 ;; winner mode
-(use-package winner
-  :bind
-  (("C-c p" . winner-undo)
-   ("C-c n" . winner-redo)
-   ))
+(global-set-key (kbd "C-c p") 'winner-undo)
+(global-set-key (kbd "C-c n") 'winner-redo)
 
 (winner-mode 1)
 
 ;; color theming
-(load-theme 'zenburn t)
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t))
 
 ;; colunm numbers
 (setq column-number-mode t)
@@ -58,49 +64,34 @@
 (define-key isearch-mode-map "\C-h" 'isearch-delete-char)
 (global-set-key [(super h)] 'help-command)
 
-;; Kill buffer closes pane
-(defun close-buffer-and-kill-window ()
-  (interactive)
-  (kill-this-buffer)
-  (if (not (one-window-p))
-      (delete-window)))
-
-(global-set-key (kbd "C-x M-k") 'close-buffer-and-kill-window)
-
 ;; ace-jump-mode
-
 (use-package ace-jump-mode
   :bind
   (("C-c <SPC>" . ace-jump-mode)
    ("C-c M-<SPC>" . ace-jump-mode-pop-mark)))
 
-;; discover-my-major
-(define-key 'help-command "\C-m" 'discover-my-major)
-(define-key 'help-command "\M-m" 'discover-my-mode)
-
-;; which-key
-(require 'which-key)
-(which-key-mode)
-(which-key-setup-side-window-right)
-(setq which-key-idle-delay 3.0)
-
 ;; Magit-status
 (global-set-key (kbd "C-x g s") 'magit-status)
 
 ;; ivy/counsel/swiper
-(ivy-mode 1)
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(use-package counsel
+  :after ivy
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-g" . counsel-rg))
+  :config (counsel-mode))
 
-(global-set-key (kbd "M-x") 'counsel-M-x)
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
 
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-
-(global-set-key (kbd "C-x C-g") 'counsel-rg)
-
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
+(use-package ivy
+  :defer 0.1
+  :diminish
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  :config (ivy-mode))
 
 ;; company
 (require 'company)
@@ -133,12 +124,6 @@
 (add-hook 'c++-mode-hook 'my-c-mode-hook)
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
-;; python-mode for SCons by default
-(add-to-list 'auto-mode-alist '("SConscript" . python-mode))
-(add-to-list 'auto-mode-alist '("SConstruct" . python-mode))
-(add-to-list 'auto-mode-alist '("Swigscript" . python-mode))
-(add-to-list 'auto-mode-alist '("Linkscript" . python-mode))
-
 ;; Markdown mode
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
@@ -167,26 +152,11 @@
 (setq lsp-ui-sideline-enable nil)
 
 ;; ace-window
-
 (require 'ace-window)
 (setq aw-keys '(?a ?e ?u ?d ?h ?t ?s))
 (setq aw-dispatch-always t)
 
-;; Quadrants
-
-(require 'windmove)
-
-(defun my/quadrants ()
-  (interactive)
-  (progn
-    (split-window-right)
-    (windmove-right)
-    (split-window-below)
-    (windmove-left)
-    (split-window-below)))
-
 ;; Key chords
-
 (defun sachachua/key-chord-define (keymap keys command)
   (if (/= 2 (length keys))
       (error "Key-chord keys must have two elements"))
@@ -208,24 +178,8 @@
 ;; Electric pairs always on
 (electric-pair-mode t)
 
-;; corral - an alternative to smartparens/paredit
-
-(require 'corral)
-
-(global-set-key (kbd "C-(") 'corral-parentheses-backward)
-(global-set-key (kbd "C-)") 'corral-parentheses-forward)
-(global-set-key (kbd "C-{") 'corral-braces-backward)
-(global-set-key (kbd "C-}") 'corral-braces-forward)
-(global-set-key (kbd "M-[") 'corral-brackets-backward)
-(global-set-key (kbd "M-]") 'corral-brackets-forward)
-(global-set-key (kbd "M-'") 'corral-double-quotes-backward)
-
 ;; use Hack as default font
 (set-frame-font "Hack")
-
-;; Custom input method for Russian
-(load-file "~/.emacs.d/russian-dvp.el")
-(setq default-input-method "russian-dvp")
 
 ;; Org mode
 (require 'org)
@@ -263,7 +217,7 @@
  '(lsp-log-io t)
  '(org-agenda-files '("~/orgfiles/life.org" "~/orgfiles/audi.org"))
  '(package-selected-packages
-   '(counsel swiper ivy eglot lsp-ui flycheck rustic with-emacs lsp-mode typescript-mode rjsx-mode solidity-mode nginx-mode yaml-mode dockerfile-mode org corral toml-mode avy ace-window ace-jump-mode key-chord typing markdown-mode which-key discover-my-major f company-racer racer cargo company geben-helm-projectile magit rust-mode csharp-mode zenburn-theme color-theme-solarized ##))
+   '(winner web-mode tide go-mode counsel swiper ivy eglot lsp-ui flycheck rustic with-emacs lsp-mode typescript-mode rjsx-mode solidity-mode nginx-mode yaml-mode dockerfile-mode org corral toml-mode avy ace-window ace-jump-mode key-chord typing markdown-mode which-key discover-my-major f company-racer racer cargo company geben-helm-projectile magit rust-mode csharp-mode zenburn-theme color-theme-solarized ##))
  '(safe-local-variable-values
    '((org-todo-keyword-faces
       ("TODO" . "red")
