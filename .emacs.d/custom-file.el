03539796ee0f1be453c3e55987dcbc0eb1dd2dce
Nikita Lesnikov <one.devl@gmail.com>
2021-09-20 18:40:34 +0300

    cleaning up using use-package

---
+++ .emacs.d/custom-file.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))
+ '(lsp-log-io t)
+ '(package-selected-packages
+   '(flycheck dockerfile-mode key-chord ace-window rustic lsp-ui lsp-mode toml-mode markdown-mode company counsel magit ace-jump-mode ##))
+ '(safe-local-variable-values
+   '((org-todo-keyword-faces
+      ("TODO" . "red")
+      ("INPROGRESS" . "orange")
+      ("DONE" . "green")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,8 @@
       '(("melpa" . 20)
 	("gnu" . 10)))
 
+(package-initialize)
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -16,7 +18,35 @@
 
 (setq use-package-always-ensure t)
 
+(setq-default
+ load-prefer-newer t
+ inhibit-startup-message t
+
+ ;; separate customization file
+ custom-file "~/.emacs.d/custom-file.el"
+
+ create-lockfiles nil
+
+ ;; indentation
+ indent-tabs-mode nil
+ default-tab-width 4
+
+ backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+
+ auto-save-default nil
+
+ enable-recursive-minibuffers t
+
+ ring-bell-function 'ignore)
+
+;; Load `custom-file` manually as we have modified the default path.
+(load-file custom-file)
+
+;; Change all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
 ;; binding & modeline
+(use-package delight)
 (use-package diminish)
 (use-package bind-key)
 
@@ -24,15 +54,6 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-
-;; org-mode by default
-(setq-default major-mode 'org-mode)
-
-;; shorter predicate
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; redirect backup directory
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 ;; winner mode
 (global-set-key (kbd "C-c p") 'winner-undo)
@@ -46,12 +67,10 @@
   :config
   (load-theme 'zenburn t))
 
-;; colunm numbers
+;; column numbers
 (setq column-number-mode t)
 
 ;; default indentation
-(setq-default default-tab-width 4)
-(setq-default indent-tabs-mode nil)
 (setq indent-line-function 'insert-tab)
 
 ;; Steve Yegge effective-emacs
@@ -70,15 +89,15 @@
   (("C-c <SPC>" . ace-jump-mode)
    ("C-c M-<SPC>" . ace-jump-mode-pop-mark)))
 
-;; Magit-status
-(global-set-key (kbd "C-x g s") 'magit-status)
+;; magit
+(use-package magit
+  :bind ("C-x g s" . magit-status))
 
 ;; ivy/counsel/swiper
 (use-package counsel
   :after ivy
   :bind (("M-x" . counsel-M-x)
-         ("C-x C-g" . counsel-rg))
-  :config (counsel-mode))
+         ("C-x C-g" . counsel-rg)))
 
 (use-package swiper
   :after ivy
@@ -86,29 +105,36 @@
          ("C-r" . swiper)))
 
 (use-package ivy
-  :defer 0.1
-  :diminish
-  :custom
-  (ivy-count-format "(%d/%d) ")
-  (ivy-use-virtual-buffers t)
-  :config (ivy-mode))
+  :config
+  (setq ivy-count-format "(%d/%d) "
+        ivy-use-virtual-buffers t
+        ivy-initial-inputs-alist nil)
+  (ivy-mode t))
 
 ;; company
-(require 'company)
-(setq company-idle-delay nil)
+(use-package company
+  :bind (:map
+         global-map
+         ("TAB" . company-complete-common-or-cycle)
+         ;; Use hippie expand as secondary auto complete. It is useful as it is
+         ;; 'buffer-content' aware (it uses all buffers for that).
+         ("M-/" . hippie-expand)
+         :map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  (setq company-idle-delay 0.1)
+  (global-company-mode t)
 
-(defun complete-or-indent ()
-  (interactive)
-  (if (company-manual-begin)
-      (company-complete-common)
-    (indent-according-to-mode)))
+  ;; Configure hippie expand as well.
+  (setq hippie-expand-try-functions-list
+        '(try-expand-dabbrev
+          try-expand-dabbrev-all-buffers
+          try-expand-dabbrev-from-kill
+          try-complete-lisp-symbol-partially
+          try-complete-lisp-symbol)))
 
-(global-set-key (kbd "C-M-/") 'complete-or-indent)
-
-;;
-
-(set-default 'truncate-lines t)
-
+;; trailing ws delete
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; cc-mode indentation & layout
@@ -125,36 +151,67 @@
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
 ;; Markdown mode
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+(use-package markdown-mode)
 
 ;; toml-mode
-(require 'toml-mode)
-(add-to-list 'auto-mode-alist '("\\.toml\\'" . toml-mode))
+(use-package toml-mode)
+
+;; flycheck
+(use-package flycheck :ensure)
+
+;; lsp
+(use-package lsp-mode
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
 
 ;; Rust
-(use-package flycheck)
-(use-package rustic)
+(use-package rustic
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
 
-(add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rustic-mode-hook))
 
-(setq rustic-lsp-client 'eglot)
-(setq rustic-lsp-server 'rust-analyzer)
-
-(setq lsp-rust-analyzer-display-chaining-hints t)
-(setq lsp-rust-analyzer-display-parameter-hints nil)
-(setq lsp-rust-analyzer-macro-expansion-method 'rustic-analyzer-macro-expand)
-(setq lsp-rust-analyzer-server-command '("~/.local/bin/rust-analyzer"))
-(setq lsp-rust-analyzer-server-display-inlay-hints nil)
-(setq lsp-rust-full-docs t)
-(setq lsp-rust-server 'rust-analyzer)
-(setq lsp-ui-doc-alignment 'window)
-(setq lsp-ui-doc-position 'top)
-(setq lsp-ui-sideline-enable nil)
+(defun rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t)))
 
 ;; ace-window
-(require 'ace-window)
-(setq aw-keys '(?a ?e ?u ?d ?h ?t ?s))
-(setq aw-dispatch-always t)
+(use-package ace-window
+  :config
+  (setq aw-keys '(?a ?e ?u ?d ?h ?t ?s))
+  (setq aw-dispatch-always t))
 
 ;; Key chords
 (defun sachachua/key-chord-define (keymap keys command)
@@ -187,11 +244,8 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 ;; (global-set-key (kbd "C-c c") 'org-capture)
 
-(setq-default major-mode 'org-mode)
-
 ;; Dockerfile mode
-(require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(use-package dockerfile-mode)
 
 ;; Allow disabling ivy completion on C-x C-f with universal argument
 ;; (for TRAMP use, for example)
@@ -204,31 +258,3 @@
     (call-interactively 'find-file)))
 
 (global-set-key (kbd "C-x C-f") 'find-file-disable-ivy)
-
-;;
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))
- '(lsp-log-io t)
- '(org-agenda-files '("~/orgfiles/life.org" "~/orgfiles/audi.org"))
- '(package-selected-packages
-   '(winner web-mode tide go-mode counsel swiper ivy eglot lsp-ui flycheck rustic with-emacs lsp-mode typescript-mode rjsx-mode solidity-mode nginx-mode yaml-mode dockerfile-mode org corral toml-mode avy ace-window ace-jump-mode key-chord typing markdown-mode which-key discover-my-major f company-racer racer cargo company geben-helm-projectile magit rust-mode csharp-mode zenburn-theme color-theme-solarized ##))
- '(safe-local-variable-values
-   '((org-todo-keyword-faces
-      ("TODO" . "red")
-      ("INPROGRESS" . "orange")
-      ("DONE" . "green")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(put 'set-goal-column 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
